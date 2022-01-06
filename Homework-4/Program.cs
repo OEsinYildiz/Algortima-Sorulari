c423b28383072eea04a5158f839db5d62a721e19
Ömer Esin <omeresinyildiz@gmail.com>
2022-01-06 13:20:59 +0100

    Homework 4

---
+++ Homework-4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz");
+            string message = Console.ReadLine();
+            string[] arr = message?.Split(' ');
+            Console.WriteLine("Toplam kelime sayısı: {0}",arr.Length);
+            string newMessage = string.Join("", arr);
+            Console.WriteLine("Toplam harf sayısı: {0}",newMessage.Length);
+        }
+    }
+}
