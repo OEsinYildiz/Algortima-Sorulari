2fba81da72d84a580b25b31630a90c9f498e187d
Ömer Esin <omeresinyildiz@gmail.com>
2022-01-06 01:09:33 +0100

    Initiliaze

---
+++ Homework-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) ThrowException.Ex();
+            Console.WriteLine($"{n} adet sayı giriniz");
+            List<int> list = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(x => Convert.ToInt32(x)).ToList();
+            if(list.Any(x=>x<0)) ThrowException.Ex();
+            foreach (var item in list)
+            {
+                if(item%2==0)
+                    Console.WriteLine(item);
+            }
+        }
+    }
+
+    static class ThrowException
+    {
+        internal static void Ex()
+        {
+            throw new Exception("Pozitif sayı girmelisiniz");
+        }
+    }
+}
