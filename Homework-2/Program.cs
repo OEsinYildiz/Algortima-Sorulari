eb8b74922696b2d935ac761c2b0e505ed22a7097
Ömer Esin <omeresinyildiz@gmail.com>
2022-01-06 01:33:34 +0100

    Homework 2

---
+++ Homework-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozitif iki sayı giriniz");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Pozitif bir sayı giriniz");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("_______________");
+            
+            foreach (var item in arr)
+            {
+                if (item%m==0)
+                {
+                    item.Console(item);
+                }
+            }
+           
+            
+            
+        }
+    }
+
+    public static class Print
+    {
+        public static void Console(this int x, int item)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+}
